b85a6a2c18d3718ed758fc42f43f5f299f72d81e
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-12-29 23:47:02 +0400

    wellcome section added

--- src/Components/Pages/Home/Sections/Building.js
+++ src/Components/Pages/Home/Sections/Building.js
@@ -3,8 +3,30 @@ import './Building.css';
 export default function Building() {
   return (
     <div className="Building">
-      <div>
+      <div className='Header'>
         <h2>A better way to start building.</h2>
+      </div>
+      <div className='Items'>
+        <div className='Item'>
+          <i class="fa-solid fa-layer-group"></i>
+          <h3>Featured title</h3>
+          <p>Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+        </div>
+        <div className='Item'>
+          <i class="fa-solid fa-layer-group"></i>
+          <h3>Featured title</h3>
+          <p>Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+        </div>
+        <div className='Item'>
+          <i class="fa-solid fa-layer-group"></i>
+          <h3>Featured title</h3>
+          <p>Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+        </div>
+        <div className='Item'>
+          <i class="fa-solid fa-layer-group"></i>
+          <h3>Featured title</h3>
+          <p>Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+        </div>  
       </div>
     </div>
   );
